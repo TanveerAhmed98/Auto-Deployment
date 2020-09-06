a286d52eada2b2729532f4d5cec9edc885fcb27f
TanveerAhmed98 <tanveer.ahm98@gmail.com>
2020-09-06 18:01:08 +0500

    Auto Deployment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Hello World learning React.JS</h1>
+      <h1>Hello World Learning React.JS</h1>
       <img src={require('./1.png')} alt="iamage not found"/>
     </div>
   );
