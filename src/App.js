92ebbe6b982a0607746a3cc091008900cbde8ca6
TanveerAhmed98 <tanveer.ahm98@gmail.com>
2020-09-06 14:58:45 +0500

    Auto Deployment

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World I am learning React.JS</h1>
+      <img src={require('./1.png')} />
+    </div>
+  );
+}
+export default App;
