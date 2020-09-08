0d40e8142150034a28330f4f3c12d22e41a9d8f8
TanveerAhmed98 <tanveer.ahm98@gmail.com>
2020-09-09 00:58:39 +0500

    Auto Deployment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Dinner from './dinner.js';
+import {Message} from './message.js';
 
 function App() {
+
+  let [count, setcount] =useState(0)
+  let [ismorning, setmorning] = useState(true)
+
   return (
-    <Dinner DishName="Chicken Biryani" AnotherDishName="Chicken Karahi"/>
+    <div id = "counter" className={`box ${ismorning ? 'daylight' : ''}`}>
+      <h1>Good {ismorning ? 'Morning' : 'Night'}</h1>
+     <Message counter={count}/>
+     <br />
+     <hr />
+     <hr />
+     <button onClick={() => setcount(++count)}>Update The Counter</button>
+     <button onClick={()=> setmorning(!ismorning)}>Update Status</button>
+    </div>
   );
+
 }
+
 export default App;
